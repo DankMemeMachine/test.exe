70d8f616e65edd87355d4ef87a52e389f4b3ad7d
dank_memes.jpg <63226@bwls.net>
2016-10-17 09:53:30 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,11 @@ function setup()
 
 function draw()
 {
-  ellipse(mouseX,mouseY,75,75);
-  
-  elispe(mouseX,mouseY,25,25);
+ellipse(200, 200, 250, 300);
+
+ellipse(200, 250, 100, 60);
+
+ellipse(150, 150, 30, 30);
+
+ellipse(250, 150, 30, 30);
 }

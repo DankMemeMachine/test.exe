962c8c206b4d85a0ca5da76661bf3fa21ddfd592
dank_memes.jpg <63226@bwls.net>
2016-10-17 10:31:57 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,19 +5,34 @@ function setup()
 
 function draw()
 {
+  fill(255, 255, 255);
  ellipse(mouseX,mouseY, 150, 150);
+  fill(255, 0, 0);
  ellipse(mouseX,mouseY, 140, 140);
+  fill(237, 60, 16);
   ellipse(mouseX,mouseY, 130, 130);
+  fill(237, 130, 16);
   ellipse(mouseX,mouseY, 120, 120);
+  fill(229, 229, 55);
   ellipse(mouseX,mouseY, 110, 110);
+  fill(151, 229, 55);
   ellipse(mouseX,mouseY, 100, 100);
+  fill(3, 150, 22);
   ellipse(mouseX,mouseY, 90, 90);
+  fill(3, 150, 74);
   ellipse(mouseX,mouseY, 80, 80);
+  fill(3, 150, 133);
   ellipse(mouseX,mouseY, 70, 70);
+  fill(3, 130, 150);
   ellipse(mouseX,mouseY, 60, 60);
+  fill(3, 54, 150);
   ellipse(mouseX,mouseY, 50, 50);
+  fill(42, 3, 150);
   ellipse(mouseX,mouseY, 40, 40);
+  fill(93, 3, 150);
   ellipse(mouseX,mouseY, 30, 30);
+  fill(52, 1, 84);
   ellipse(mouseX,mouseY, 20, 20);
+  fill(0, 0, 0);
   ellipse(mouseX,mouseY, 10, 10);
 }

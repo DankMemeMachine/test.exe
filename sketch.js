08d42587dc03074cf77b6a3101755384e2f47103
dank_memes.jpg <63226@bwls.net>
2016-10-18 13:21:00 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,36 +7,8 @@ var y=550
 function draw()
 {
   background(28, 44, 68);
-  fill(255, 255, 255);
- ellipse(x,y, 150, 150);
-  fill(255, 0, 0);
- ellipse(x,y, 140, 140);
-  fill(237, 60, 16);
-  ellipse(x,y, 130, 130);
-  fill(237, 130, 16);
-  ellipse(x,y, 120, 120);
-  fill(229, 229, 55);
-  ellipse(x,y, 110, 110);
-  fill(151, 229, 55);
-  ellipse(x,y, 100, 100);
-  fill(3, 150, 22);
-  ellipse(x,y, 90, 90);
-  fill(3, 150, 74);
-  ellipse(x,y, 80, 80);
-  fill(3, 150, 133);
-  ellipse(x,y, 70, 70);
-  fill(3, 130, 150);
-  ellipse(x,y, 60, 60);
-  fill(3, 54, 150);
-  ellipse(x,y, 50, 50);
-  fill(42, 3, 150);
-  ellipse(x,y, 40, 40);
-  fill(93, 3, 150);
-  ellipse(x,y, 30, 30);
-  fill(52, 1, 84);
-  ellipse(x,y, 20, 20);
-  fill(0, 0, 0);
-  ellipse(x,y, 10, 10);
+  rect(x,y,100,200)
+  triangle(x1,y1,x2,y2,x3,y3)
   x=x+5
   y=y-3
 }

f5f398283de83059d7cc7701ff301328b918db63
dank_memes.jpg <63226@bwls.net>
2016-10-18 10:48:40 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,38 +2,38 @@ function setup()
 {
   createCanvas(windowWidth,windowHeight);
 }
-
+var x=20
 function draw()
 {
   background(255,255,255);
   fill(255, 255, 255);
- ellipse(mouseX,mouseY, 150, 150);
+ ellipse(mouseX,200, 150, 150);
   fill(255, 0, 0);
- ellipse(mouseX,mouseY, 140, 140);
+ ellipse(mouseX,200, 140, 140);
   fill(237, 60, 16);
-  ellipse(mouseX,mouseY, 130, 130);
+  ellipse(mouseX,200, 130, 130);
   fill(237, 130, 16);
-  ellipse(mouseX,mouseY, 120, 120);
+  ellipse(mouseX,200, 120, 120);
   fill(229, 229, 55);
-  ellipse(mouseX,mouseY, 110, 110);
+  ellipse(mouseX,200, 110, 110);
   fill(151, 229, 55);
-  ellipse(mouseX,mouseY, 100, 100);
+  ellipse(mouseX,200, 100, 100);
   fill(3, 150, 22);
-  ellipse(mouseX,mouseY, 90, 90);
+  ellipse(mouseX,200, 90, 90);
   fill(3, 150, 74);
-  ellipse(mouseX,mouseY, 80, 80);
+  ellipse(mouseX,200, 80, 80);
   fill(3, 150, 133);
-  ellipse(mouseX,mouseY, 70, 70);
+  ellipse(mouseX,200, 70, 70);
   fill(3, 130, 150);
-  ellipse(mouseX,mouseY, 60, 60);
+  ellipse(mouseX,200, 60, 60);
   fill(3, 54, 150);
-  ellipse(mouseX,mouseY, 50, 50);
+  ellipse(mouseX,200, 50, 50);
   fill(42, 3, 150);
-  ellipse(mouseX,mouseY, 40, 40);
+  ellipse(mouseX,200, 40, 40);
   fill(93, 3, 150);
-  ellipse(mouseX,mouseY, 30, 30);
+  ellipse(mouseX,200, 30, 30);
   fill(52, 1, 84);
-  ellipse(mouseX,mouseY, 20, 20);
+  ellipse(mouseX,200, 20, 20);
   fill(0, 0, 0);
-  ellipse(mouseX,mouseY, 10, 10);
+  ellipse(mouseX,200, 10, 10);
 }

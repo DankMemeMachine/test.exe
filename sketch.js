6f0f504bff1e2e691eefbadf187984aba6ca3d2e
dank_memes.jpg <63226@bwls.net>
2016-10-18 13:24:38 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,11 +4,15 @@ function setup()
 }
 var x=20
 var y=550
+var w=100
+var h=200
 function draw()
 {
   background(28, 44, 68);
   fill(227, 228, 229);
-  rect(x,y,100,200);
+  rect(x,y,w,h);
   x=x+5;
   y=y-3;
+  h=h-1
+  w=w-1
 }

63d5ac05f9c8cbb72283f051bb2312276035b963
dank_memes.jpg <63226@bwls.net>
2016-10-26 16:18:25 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,17 +2,8 @@ function setup()
 {
   createCanvas(windowWidth,windowHeight);
 }
-var x=300
-var y=300
 
 function draw()
 {
-    rotate(45)
-  background(28, 44, 68);
-  fill(227, 228, 229);
-  rect(x,y,100,200);
-  fill(163, 238, 255);
-  ellipse(x+50,y+80,70,70);
-  x=x+2;
-  y=y-7;
+rect(900,900,900,windowWidth
 }

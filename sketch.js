53b97bf477da2c42aa8d2217517666ee07fc7731
dank_memes.jpg <63226@bwls.net>
2016-10-26 16:32:17 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ function setup()
 function draw()
 {
   fill(122, 122, 122);
-rect(1,600,windowWidth,100);
+rect(1,600,windowWidth-1,100);
     fill(122, 122, 122);
   triangle(1,600,100,500,100,600);
     fill(122, 122, 122);

59302a2c3740b80b4711ea5aa3b522c1cbe6409a
dank_memes.jpg <63226@bwls.net>
2016-10-26 16:35:36 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,4 +14,7 @@ rect(1,600,windowWidth-1,100);
   fill(122, 122, 122);
   stroke(122,122,122);
   triangle(windowWidth-1,600,windowWidth-100,500,windowWidth-100,600);
+    fill(122, 122, 122);
+  stroke(122,122,122);
+  rect(100,500,windowWidth-100,100);
 }
